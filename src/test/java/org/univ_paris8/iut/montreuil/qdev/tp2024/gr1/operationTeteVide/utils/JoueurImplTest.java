ecafd62b3483849aa4399e9453a4a61d0987a3ca
emarielouise972 <marielouiseesteban90@gmail.com>
2024-03-26 14:22:37 +0100

    last test

--- src/test/java/org/univ_paris8/iut/montreuil/qdev/tp2024/gr1/operationTeteVide/utils/JoueurImplTest.java
+++ src/test/java/org/univ_paris8/iut/montreuil/qdev/tp2024/gr1/operationTeteVide/utils/JoueurImplTest.java
@@ -14,7 +14,7 @@ import org.junit.jupiter.api.Assertions.*;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.AssertThrows.assertThrows;
+//import static org.junit.jupiter.api.AssertThrows.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class JoueurImplTest {
